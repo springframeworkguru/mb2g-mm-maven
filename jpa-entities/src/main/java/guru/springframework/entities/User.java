48bd4b99a0c30a9f9a0b4e7cd0f608039d917aae
John Thompson <john@springframework.guru>
2018-12-15 17:21:42 -0500

    adding entities module

---
+++ jpa-entities/src/main/java/guru/springframework/entities/User.java
@@ -0,0 +1,28 @@
+package guru.springframework.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by jt on 2018-12-15.
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class User {
+
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
